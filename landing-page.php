f6b51dcc1fb47fd9b4db1d308d7b386844528779
Rodrigo <rodrigo.ross.comp@gmail.com>
2018-09-17 16:06:15 -0400

    Initial commit

---
+++ landing-page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta charset="utf-8">
+    <meta name="keywords" content="awati">
+    <meta name="keywords" content="sucupira">
+    <meta name="keywords" content="computação em nuvem, consultoria, outsourcing, implantações, TI">
+    <meta name="description" content="Awati tecnologia planejada, &eacute; uma empresa de TI que opera em Cuiab&aacute;/V&aacute;rzea Grande, prestando servi&ccedil;os de consultoria, outsourcing e implanta&ccedil;&otilde;es para empresas nas mais diversos ramos de trabalho.">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="Computador por assinatura">
+    <title>Computador por assinatura</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="imagem/png" href="images/icons/favicon.png" />
+    <!-- Booststrap 4.0 -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <!--Author Stylesheet-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    
+</body>
+</html>
